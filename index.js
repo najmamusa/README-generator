54c3d2fcddb1bbccfd1bb063626fb8736a0c59ec
Najima Musa <najmusaa@gmail.com>
2024-02-05 11:55:25 +0000

    adding all the changes

---
+++ index.js
@@ -0,0 +1,189 @@
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
+const questions = [
+  {
+    type: 'input',
+    name: 'user',
+    message: `What is your GitHub username?`,
+    when: (resp) => !resp.cancelled,
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: `What is your contact email address for any questions about the project?`,
+    validate: function (value) {
+      let valid = value.match(/^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/);
+      if (valid) {
+        return true;
+      }
+      return `Please add a valid email address`;
+    },
+  },
+  {
+    type: 'input',
+    name: 'repo',
+    message: `What is the name for the GitHub repository?`,
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: `How is this project licenced?`,
+    choices: [
+      'none',
+      'MIT',
+      'APACHE2.0',
+      'GPL3.0',
+      'BSD2',
+      'BSD3',
+      'LGPL3.0',
+      'MPL2.0',
+      'CDDL1.0',
+      'EPL2.0',
+      'AGPL3.0',
+      'WTFPL',
+      'ISC',
+      'CC01.0',
+      'UNLICENSED',
+    ],
+  },
+  {
+    type: 'confirm',
+    name: 'deployment',
+    message: `Is your project deployed?`,
+  },
+  {
+    type: 'input',
+    name: 'title',
+    message: `What is the name of your project?`,
+  },
+  {
+    type: 'input',
+    name: 'subtitle',
+    message: `If you'd like, you can add a subtitle to the project.`,
+  },
+  {
+    type: 'input',
+    name: 'about',
+    message: `Provide a description explaining the what, why, and how of your project.`,
+  },
+  {
+    type: 'confirm',
+    name: 'includeInstallation',
+    message: `Would you like to include installation guide?`,
+    default: true,
+  },
+  {
+    type: 'editor',
+    name: 'installation',
+    message: `Enter project installation guide:`,
+    when: (resp) => resp.includeInstallation,
+  },
+  {
+    type: 'input',
+    name: 'screenshot',
+    message: `Do you have a screenshot? If yes, please provide the relative path to the image you want to use as the screenshot.`,
+  },
+  {
+    type: 'confirm',
+    name: 'includeFeatures',
+    message: `Would you like to include project features?`,
+    default: true,
+  },
+  {
+    type: 'editor',
+    name: 'features',
+    message: `Enter the project features (one per line):`,
+    when: (resp) => resp.includeFeatures,
+  },
+  {
+    type: 'confirm',
+    name: 'includeCredits',
+    message: `Would you like to include credits section?`,
+    default: true,
+  },
+  {
+    type: 'editor',
+    name: 'credits',
+    message: `List all contributors, links to resources, etc. (one per line)`,
+    when: (resp) => resp.includeCredits,
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: `If you'd like to include tests section, describe your tests here.`,
+  },
+  {
+    type: 'confirm',
+    name: 'contribute',
+    message: `Would you like to specify a contribution section with generic details how to contribute to this project?`,
+  },
+  {
+    type: 'confirm',
+    name: 'includeRoadmap',
+    message: 'Do you want to include future development roadmap section?',
+    default: true,
+  },
+  {
+    type: 'editor',
+    name: 'roadmap',
+    message: `If you have future roadmap for this project, you can specify a future development steps here (one per line)`,
+    when: (resp) => resp.includeRoadmap,
+  },
+  {
+    type: 'confirm',
+    name: 'toc',
+    message: `Would you like to include a table of contents?`,
+  },
+];
+
+// write README file
+function writeToFile(fileName, resp) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), resp);
+}
+
+function init() {
+  const cancelConfirmation = {
+    type: 'confirm',
+    name: 'cancelled',
+    message: `Are you sure you want to cancel creating the README?`,
+  };
+
+  const prompt = inquirer.prompt(questions);
+
+  function cancel(str, key) {
+    if (key.name === 'escape') {
+      prompt.ui.close();
+      inquirer.prompt(cancelConfirmation).then((resp) => {
+        if (resp.cancelled) {
+          process.exit();
+        } else {
+          prompt.ui = new inquirer.ui.Prompt(prompt.resp, prompt.opts);
+          prompt.run().catch(() => {});
+        }
+      });
+    }
+  }
+
+  process.stdin.on('keypress', cancel);
+
+  return prompt
+    .then((resp) => {
+      writeToFile(`./generated-readme/README.md`, generateMarkdown(resp));
+      console.log(
+        `Your README.md has been created.✔️  Find it at ${path.join(
+          __dirname,
+          'generated-readme',
+          'README.md'
+        )}`
+      );
+    })
+    .catch((error) => {
+      if (error.message !== 'README generation cancelled') {
+        console.error(error);
+      }
+    });
+}
+init();
---
+++ utils/generateMarkdown.js
@@ -0,0 +1,104 @@
+function generateMarkdown(resp) {
+    const year = new Date().getFullYear();
+    let markdown = `# ${resp.title ? resp.title : resp.repo}\n\n`;
+    resp.subtitle && (markdown += `${resp.subtitle}\n\n`);
+  
+    // licence badge
+    resp.license !== 'none' &&
+      (markdown += `![License](https://img.shields.io/badge/license-${resp.license}-3558AE.svg?labelColor=2F2E32&style=for-the-badge&logo=simple-icons)\n\n`);
+  
+    // about
+    resp.about && (markdown += `## About\n\n${resp.about}\n\n`);
+  
+    // table of contents
+    if (resp.toc) {
+      markdown += `## Table of Contents\n\n`;
+      resp.installation && (markdown += `- [Installation](#installation)\n`);
+      resp.features && (markdown += `- [Features](#features)\n`);
+      resp.credits && (markdown += `- [Credits](#credits)\n`);
+      resp.tests && (markdown += `- [Tests](#tests)\n`);
+      resp.roadmap &&
+        (markdown += `- [Future Development](#future-development)\n`);
+      resp.contribute && (markdown += `- [Contributing](#contributing)\n`);
+      resp.license !== 'none' && (markdown += `- [License](#license)\n`);
+      resp.email && (markdown += `- [Contact](#contact)\n`);
+      markdown += '\n';
+    }
+  
+    // installation
+    resp.installation &&
+      (markdown += `## Installation\n\n${resp.installation}\n\n`);
+  
+    // deployment
+    resp.deployment &&
+      (markdown += `### [View Project :star:](https://${resp.user}.github.io/${resp.repo})\n\n`);
+  
+    // screenshot
+    resp.screenshot && (markdown += `![Screenshot](${resp.screenshot})\n`);
+  
+    // features
+    resp.features &&
+      (markdown += `
+  ## Features\n
+  ${resp.features
+    .split('\n')
+    .map((feature) => feature.trim())
+    .filter(Boolean) // removes empty lines
+    .map((feature) => `- [x] ${feature}`)
+    .join('\n')}\n`);
+  
+    // credits
+    resp.credits &&
+      (markdown += `
+  ## Credits\n
+  ${resp.credits
+    .split('\n')
+    .map((credit) => credit.trim())
+    .filter(Boolean)
+    .map((credit) => `- ${credit}`)
+    .join('\n')}\n`);
+  
+    // tests
+    resp.tests && (markdown += `## Tests\n\n${resp.tests}\n\n`);
+  
+    // roadmap
+    resp.roadmap &&
+      (markdown += `
+  ## Future Development\n
+  ${resp.roadmap
+    .split('\n')
+    .map((dev) => dev.trim())
+    .filter(Boolean)
+    .map((dev) => `- ${dev}`)
+    .join('\n')}\n`);
+  
+    // contributions
+    resp.contribute &&
+      (markdown += `
+  ## Contributing\n
+  Contributions are always welcome!\n
+  [![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-2.1-4baaaa.svg?labelColor=2F2E32&style=for-the-badge&logo=simple-icons)](https://www.contributor-covenant.org/version/2/1/code_of_conduct/)\n
+  ### Creating A Pull Request\n
+  - Fork the project
+  - Create your feature branch (\`git checkout -b feature/newfeature\`)
+  - Commit your changes (\`git commit -m 'Add feature'\`)
+  - Push to the branch (\`git push origin feature/newfeature\`)
+  - Open a Pull Request\n\n`);
+  
+    // licence
+    resp.license !== 'none' &&
+      (markdown += `## License\n\nThis application is covered by the ${resp.license} license. \n\n`);
+  
+    // questions
+    resp.email &&
+      (markdown += `## Contact\n
+  If you have any questions, please contact me at ${resp.email}. Thank you!\n\n`);
+  
+    // copyright
+    markdown += `Copyright &copy; ${year} [:octocat:@${resp.user}](https://github.com/${resp.user})\n`;
+  
+    markdown += `\n\n<hr><p style="text-align:right; font-style:italic">This README was generated with ❤️ by <a href="https://github.com/alexandrabatrak/readme-generator">readme-generator</a></p>`;
+    return markdown;
+  }
+  
+  module.exports = generateMarkdown;
